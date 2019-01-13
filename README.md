484d7d273821aed97a994669b3b12ba1965b0ac2
Zack Rauen <rauenzi@outlook.com>
2019-01-13 16:24:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Nox - [Download](http://betterdiscord.net/ghdl/?url=https://raw.githubusercontent.com/rauenzi/Nox/master/release/Nox.theme.css)
+
+A beautiful dark material theme for Discord. This is the rebrand and continuation of Beard's Material Design Theme which has been discontinued.
+
+For support and to report bugs, please visit the [support server](http://bit.ly/BMTserver).
+
+# Variables
+
+These variables allow you to customize the theme to your preferences.
+
+ - `--nox-accent` - The accent color you want for the theme. Default: `#3F85FF`
+ - `--nox-notification-color` - Color for the unread indicator in guilds list. Default: `#FFFFFF`
+ - `--nox-notification-width` - Width of the circle around guilds. Default: `3px`
+ - `--nox-important-notification` - Background color for mention/ping badges. Default: `#F04747`
+ - `--nox-channels-scrollbar` - Set this to `none` to remove the scrollbar in the channel list.
+ - `--nox-settings-scrollbar` - Set this to `none` to remove the scrollbar in settings.
+ - `--nox-settings-time` - Length of the animation to open settings. Default: `500ms`
+ - `--nox-level1` - One of the main grays used throughout the theme. Default: `#303030`
+ - `--nox-level2` - One of the main grays used throughout the theme. Default: `#212121`
+ - `--nox-level3` - One of the main grays used throughout the theme. Default: `#141414`
+
+ # Addons
+
+There are a number of addons and customizations available for the theme that can be found on the [support server](http://bit.ly/BMTserver).
+
+# Previews
+
+## Main Chat
+![Main Chat](https://i.zackrauen.com/K4Vt3Y.png)
+
+## User Popout
+![User Popout](https://i.zackrauen.com/jxCR54.png)
+
+## User Profile
+![User Profile](https://i.zackrauen.com/XF0k1X.png)
+
+## Status Picker
+![Status Picker](https://i.zackrauen.com/LJ7LLX.png)
+
+## Store
+![Store](https://i.zackrauen.com/MraKGe.png)
+
+## Settings
+![Settings](https://i.zackrauen.com/vGEeJs.png)
+
+## Notification Settings
+![Notification Settings](https://i.zackrauen.com/McBetP.png)
---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const gulp = require("gulp");
+const fs = require("fs");
+const path = require("path");
+const sass = require('gulp-sass');
+const rename = require("gulp-rename");
+const insert = require("gulp-insert");
+const globImporter = require('node-sass-glob-importer');
+
+const META = `/*//META{"name":"Nox","description":"A theme for Discord loosely based on Google's Material Design Guidelines.","author":"Lilian Tedone & Zerebos","version":"2.2.6"}*//**/`;
+
+const LICENSE = `/*
+ *  Copyright (c) 2016-2017 Lilian Tedone, 2017-2019 Zack Rauen
+ * 
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+`;
+
+const isDirectory = source => fs.lstatSync(source).isDirectory();
+const getDirectories = source => fs.readdirSync(source).map(name => path.join(source, name)).filter(isDirectory);
+const directories = getDirectories("./src").map(d => `./${d}/*.scss`.replace("\\", "/"));
+
+const fileList = ["./src/index.css", "./src/*.scss", ...directories];
+
+gulp.task('import', function () {
+  return gulp.src('./src/index.scss')
+    .pipe(sass({importer: globImporter()}).on('error', sass.logError))
+    .pipe(rename({basename: "import", extname: ".css"}))
+    .pipe(insert.prepend(LICENSE))
+    .pipe(gulp.dest('./release/'));
+});
+ 
+gulp.task('sass', function () {
+  return gulp.src('./src/index.scss')
+    .pipe(sass({importer: globImporter()}).on('error', sass.logError))
+    .pipe(rename({basename: "Nox.theme", extname: ".css"}))
+    .pipe(insert.prepend(META + LICENSE))
+    .pipe(gulp.dest("Z:/Programming/BetterDiscordStuff/themes"));
+});
+
+gulp.task("sass-watch", function() {
+  return gulp.watch(fileList, ["sass"]);
+});
